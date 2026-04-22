97f9007a265c971db83b92877e6a5ed1c4c28281
Berk Özkütük <berk@ozkutuk.me>
2026-04-23 01:13:06 +0200

    options: highlight on yank

--- nvim/lua/extra.lua
+++ nvim/lua/extra.lua
@@ -24,6 +24,17 @@ vim.api.nvim_create_autocmd("CompleteDone", {
   end,
 })
 
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('my.highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 vim.filetype.add {
   filename = {
     ["todo.txt"] = "todotxt",
