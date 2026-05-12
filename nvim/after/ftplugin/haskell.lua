599503773277fcc4b845da1c7d8bddc09cb63515
Berk Özkütük <berk@ozkutuk.me>
2026-05-12 11:51:09 +0200

    ftplugin(haskell): use treesitter indentation instead of haskell-vim

--- nvim/after/ftplugin/haskell.lua
+++ nvim/after/ftplugin/haskell.lua
@@ -1,1 +1,7 @@
 vim.bo.shiftwidth = 2
+
+-- disable indentation from haskell-vim
+vim.g.haskell_indent_disable = 1
+
+-- ...use treesitter indentation instead
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
